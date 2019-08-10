e224f19d894987d67b855aa2742c1ed5b0dd13dd
Gustavo Matos <37024361+guMatos@users.noreply.github.com>
2019-08-10 12:19:12 -0300

    Creates monitor controller

---
+++ src/Incredibilis.Presentation/Controllers/MonitorController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Incredibilis.Presentation.Controllers
+{
+	[ApiController]
+	[Route("")]
+	public class MonitorController : ControllerBase
+	{
+		[HttpGet]
+		[Route("[action]")]
+		public IActionResult Health()
+			=> Ok("UP");
+	}
+}
